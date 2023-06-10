6ed2ecb13f5e6b1dce0b321fabd93f9edd2256da
xijie <xijie@sit.com.cn>
2023-06-11 00:44:10 +0800

    Initial project

---
+++ src/main/java/com/xj/demo/MainApplication.java
@@ -0,0 +1,18 @@
+package com.xj.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xijie
+ * @version V1.0
+ * Date 2023/6/11 0:07
+ * @Description:
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
